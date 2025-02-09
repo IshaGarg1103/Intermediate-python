9f593189ff3666fec184975593da0c0ec5c9b311
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-02-10 00:58:55 +0530

    deep copy

--- dictionary/dict2.py
+++ dictionary/dict2.py
@@ -8,7 +8,13 @@ print(mydict_cpy) #hence, changes made in dict 1 can be seen in 2nd dictionary
 print(mydict) #both the dictionaries will change
 
 mydict={"name":"isha","age":20,"city":"new york"}
-mydict_cpy=dict(mydict)
+mydict_cpy=dict(mydict)#making shallow copy
 mydict_cpy["email"]="max@yz.com"
-print(mydict_cpy)
+print(mydict_cpy) #here the changes in one dictionary doesnt make any in 2nd dictionary except lists
+print(mydict)
+
+mydict={"name":"isha","age":20,"city":"new york"}
+mydict_cpy=mydict.copy() #making deep copy
+mydict_cpy["email"]="max@yz.com"
+print(mydict_cpy) #here the changes in one dictionary doesnt make any in 2nd dictionary
 print(mydict)
