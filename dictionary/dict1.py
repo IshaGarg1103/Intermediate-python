e8be310aa770d6c2d23672ee3853dcd57e556604
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-02-09 23:01:54 +0530

    dictionary intro

---
+++ dictionary/dict1.py
@@ -0,0 +1,28 @@
+#dictionary : key -value pairs, unordered, mutable
+mydict={"name":"max","age":28,"city":"new york"}
+print(mydict)
+
+mydict2=dict(name="marry",age=27,city="boston")
+print(mydict2)
+value=mydict["age"]
+print(value)
+
+#value=mydict["lastname"]
+#print(mydict) #keyerror : 'lastname'
+
+#appending an element
+mydict["email"]="max@xyz.com"
+print(mydict)
+
+try:
+    print(mydict['lastname'])
+except:
+    print("Error") #will produce an error, since no key named lastname is present in mydict
+
+#acquiring keys in dictionary
+for value in mydict:
+    print(value,end=' ') #will give keys in the output
+    #ouput:name age city email
+
+for key,value in mydict.items():
+    print(key,value)
