45ec0e746e2c31e4a8eda16d237b201af4f2e1aa
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-02-10 20:38:28 +0530

    deep copy intro in dict2

--- dictionary/dict2.py
+++ dictionary/dict2.py
@@ -1,7 +1,8 @@
-
+import copy
 mydict={"name":"isha","age":20,"city":"new york"}
 print(mydict)
 
+#shallow copy
 mydict_cpy=mydict #both of them are pointing to the same dictionary
 mydict_cpy["email"]="max@yz.com"
 print(mydict_cpy) #hence, changes made in dict 1 can be seen in 2nd dictionary
@@ -14,7 +15,14 @@ print(mydict_cpy) #here the changes in one dictionary doesnt make any in 2nd dic
 print(mydict)
 
 mydict={"name":"isha","age":20,"city":"new york"}
-mydict_cpy=mydict.copy() #making deep copy
+mydict_cpy=mydict.copy() #making shallow copy method 2
 mydict_cpy["email"]="max@yz.com"
-print(mydict_cpy) #here the changes in one dictionary doesnt make any in 2nd dictionary
+print(mydict_cpy) 
+print(mydict)
+
+#deep copy
+mydict={"name":"isha","age":20,"city":"new york","scores" : [10,20,30]}
+mydict_cpy=copy.deepcopy(mydict) #making deepcopy
+mydict_cpy["scores"][1]=10
+print(mydict_cpy) 
 print(mydict)
