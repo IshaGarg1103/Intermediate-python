12e3a4767c410e197c8966a61a4239b0651929fc
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-02-12 23:52:19 +0530

    addition of tuple as key in dictionary

--- dictionary/dict3.py
+++ dictionary/dict3.py
@@ -5,7 +5,24 @@ my_dict_2=dict(name="Marry",age=27,city="boston")
 mydict.update(my_dict_2)
 print(mydict)
 
-#
-my_dict_3=dict(email="maxton@xyz.com")
+#new attribute can be added directly to the dictionary this way
+my_dict_3=dict(major="compsci")
 mydict.update(my_dict_3)
+print(mydict)
+
+my_dict={3:9,6:36,9:81}
+print(my_dict)
+
+#value=my_dict[0]
+#print(value) #Keyerror:0 
+#use key directly to obtain the value, instead of index no.
+
+mytuple=[8,7]
+#mydict={mytuple:15}
+#print(mydict) #Type Error : unhashable type : 'list' 
+#In Python, dictionary keys must be hashable and immutable, meaning they must 
+# have a fixed value that doesn’t change over time
+
+mytuple=(8,7)
+mydict={mytuple : 15}
 print(mydict)
