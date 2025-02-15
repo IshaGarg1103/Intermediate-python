88bf8b341bd311cbb04417d50d940344d5ebdfb8
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-02-15 13:52:29 +0530

    creating copies in sets

--- set/set4.py
+++ set/set4.py
@@ -1,5 +1,20 @@
 set1={1,2,3,4}
+
 set2=set1
 set2.add(5)
-print(set1)
-print(set2)
+print(set1) #{1,2,3,4,5}
+print(set2) #{1,2,3,4,5}
+
+#craeting copies
+
+#method-1
+set2=set1.copy()
+set1.add(6)
+print(set1) #{1,2,3,4,5,6}
+print(set2) #{1,2,3,4,5}
+
+#method-2
+set3=set(set1)
+set3.add(7)
+print(set1) #{1,2,3,4,5,6,7}
+print(set3) #{1,2,3,4,5}
