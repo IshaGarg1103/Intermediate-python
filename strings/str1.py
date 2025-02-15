9c161b9906bb94cd78a1ad2e66876831fd5da8fa
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-02-15 20:44:15 +0530

    string timer

--- strings/str1.py
+++ strings/str1.py
@@ -1,3 +1,5 @@
+from timeit import default_timer as timer
+
 str='how are you doing ?'
 mylist=str.split()
 print(mylist) #['how', 'are', 'you', 'doing', '?']
@@ -10,4 +12,19 @@ mylist=str.split(",")
 print(mylist) #['how', 'are', 'you', 'doing?']
 
 newstr=' '.join(mylist)
-print(newstr)
+print(newstr) #how are you doing?
+
+#bad method to concatenate
+start=timer()
+mylist=['a']*100000
+newstr=''
+for i in mylist:
+    newstr+=i
+stop=timer()
+print(stop-start)
+
+#GOOD METHOD :
+start=timer()
+newstr=''.join(mylist)
+stop=timer()
+print(stop-start)
