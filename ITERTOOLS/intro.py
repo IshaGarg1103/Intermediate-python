aff37d3065681e54bd7c0fc0e7e514e6f5d245ca
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-02-18 23:28:41 +0530

    itertools intro

--- ITERTOOLS/intro.py
+++ ITERTOOLS/intro.py
@@ -2,5 +2,6 @@
 from itertools import product
 a = [1,2]
 b = [3,4]
-prod = product(a,b)
-print(list(prod))
+prod = product(a,b,repeat=1)
+print(list(prod)) #[(1, 3), (1, 4), (2, 3), (2, 4)]
+print("hello")
