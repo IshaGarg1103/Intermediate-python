9345f0384f5c8f4c4e7365dfd83e9c85072ce9ba
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-02-18 23:46:08 +0530

    .

--- ITERTOOLS/intro.py
+++ ITERTOOLS/intro.py
@@ -1,7 +1,24 @@
+from itertools import count, cycle, repeat, product
 #itertools : product, permutations, combinations, accumulate, groupby, and infinite iterators.
-from itertools import product
+
 a = [1,2]
 b = [3,4]
 prod = product(a,b,repeat=1)
 print(list(prod)) #[(1, 3), (1, 4), (2, 3), (2, 4)]
-print("hello")
+
+
+
+# Infinite counter
+for i in count(10, 2):
+    print(i, end =" ")
+    if i > 20:
+        break
+print("\n")
+# Cycling through elements
+cycler = cycle("ABC")
+for _ in range(5):
+    print(next(cycler))
+
+# Repeating an element
+repeater = repeat("Hello", 3)
+print(list(repeater))
