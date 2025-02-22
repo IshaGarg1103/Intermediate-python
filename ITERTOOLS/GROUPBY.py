20e53ff85542f2f3e6e3fda513bbd4fcd1e1d8c4
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-02-22 23:52:48 +0530

    lambda

--- ITERTOOLS/GROUPBY.py
+++ ITERTOOLS/GROUPBY.py
@@ -1,9 +1,13 @@
 from itertools import groupby
 
 def smaller_than3(x):
-    return x<4  #returns True for x<3
+    return x<4   #returns True for x<3
 
 a = [1,2,3,4]
 group_obj = groupby(a, key = smaller_than3)
 for key, value in group_obj:
+    print(key, list(value))
+
+group_obj1=groupby(a, key=lambda x : x<3)
+for key, value in group_obj1:
     print(key, list(value))
